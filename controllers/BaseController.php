1ee1d09209e6ae1553885fd6517b55d67df53246
RostyslavLu <133259169+RostyslavLu@users.noreply.github.com>
2023-07-13 12:18:45 -0400

    Update BaseController.php

--- controllers/BaseController.php
+++ controllers/BaseController.php
@@ -1,9 +1,6 @@
 <?php
 
 function base_controller_index() {
-    //
-    //http://localhost:7080/2395286/MVC/index.php?module=base&action=index
-    //echo 'Base';
     render(VIEW_DIR.'/base/welcome.php');
 }
 
